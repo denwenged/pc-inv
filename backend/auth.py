516761e9c2a6a8f60f9d25100607815c08bbe98c
denwenged <astonweb6@gmail.com>
2026-09-06 14:24:09 +0200

    docker bullshit fix

--- backend/auth.py
+++ backend/auth.py
@@ -4,19 +4,19 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-import os
 
-# Lee la clave de la variable de entorno, si no existe usa una por defecto
-SECRET_KEY = os.getenv("JWT_SECRET", "clave_super_secreta_por_defecto_123")
+SECRET_KEY = os.getenv("JWT_SECRET", "clave_super_secreta_123")
 ALGORITHM = "HS256"
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def create_access_token(data: dict):
     to_encode = data.copy()
